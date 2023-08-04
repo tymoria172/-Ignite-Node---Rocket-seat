f241f401885936bc43a45c64b0dc38d0c0290a65
andre172 <andre.ricardo2115@gmail.com>
2023-08-04 12:31:40 -0300

    Projeto concluido

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,23 +1,26 @@
 import http from "http";
+import { routes } from "./routes.js";
+import { extractQueryParams } from './utils/extract-query-params.js'
+import { json } from "./middlewares/json.js";
 
-const users = [];
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
-  }
+  await json(req, res);
 
-  if (method === "POST" && url === "/users") {
-    users.push({
-      id: 1,
-      name: "Jonny",
-      email: "jonny@email.com.br",
-    });
-    return res.writeHead(201).end();
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
+
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end();
--- 01-fundamentos-nodejs/streams/stream-http-server.js
+++ 01-fundamentos-nodejs/streams/stream-http-server.js
@@ -3,7 +3,7 @@ import { Transform } from 'node:stream';
 
 class InverseNumberStream extends Transform {
     _transform(chunk, encoding, callback) {
-        const transformed = (Number(chunk.toString()) * -1)
+        const transformed = Number(chunk.toString()) * -1
 
         console.log(transformed);
         callback(null, Buffer.from(String(transformed)))
